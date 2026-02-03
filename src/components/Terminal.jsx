6faac7b4990a626e7f7cb6379a0ec440b0f9b6c5
Christian Egelund <christianegelundhansen@hotmail,com>
2026-02-03 21:17:22 +0100

    npm builæd

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -140,7 +140,7 @@ function handleCommand(term, input) {
             "Bla. Bla. Bla fill in"
         )
         } else if (args[0] === "MyCV.txt"){
-            term.writeln("Here is my CV. Test, test")
+            term.writeln("Here is my CV. Test, test, test")
         } else {
         term.writeln("cat: file not found")
         }
